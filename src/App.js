3def4ed80f18b6c732c3421b475ecfa605e893ef
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-03-30 23:27:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { Route } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+import { Switch } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+
+function App() {
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </Layout>
+  );
+}
+
+export default App;
